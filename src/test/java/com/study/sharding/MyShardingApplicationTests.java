9f9e4af41db8e9bf960506b449f23600280eb468
pukaiquan <1137580844@qq.com>
2019-07-17 11:34:59 +0800

    up

--- src/test/java/com/study/sharding/MyShardingApplicationTests.java
+++ src/test/java/com/study/sharding/MyShardingApplicationTests.java
@@ -1,44 +1,34 @@
 package com.study.sharding;
 
-import com.mongodb.WriteResult;
-import com.mongodb.client.result.DeleteResult;
 import com.study.sharding.entity.TOrder;
 import com.study.sharding.mapper.OrderMapper;
 import com.study.sharding.service.OrderService;
-import com.study.sharding.util.Sequence;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MyShardingApplicationTests {
-	@Autowired
-	private MongoTemplate mongoTemplate;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-	@Autowired
-	OrderService orderService;
+    @Autowired
+    OrderService orderService;
 
-	@Autowired
-	OrderMapper orderMapper;
+    @Autowired
+    OrderMapper orderMapper;
 
-	@Test
-	public void contextLoads() {
-		TOrder order=new TOrder();
+    @Test
+    public void contextLoads() {
+        TOrder order = new TOrder();
 //		List<TOrder> list = orderMapper.selectById(order);
 //		order.setOrderId(Sequence.getSequenceId());
 //		order.setUserId(Sequence.getSequenceId());
@@ -46,7 +36,7 @@ public class MyShardingApplicationTests {
 //		order.setStatus(1);
 //		mongoTemplate.insert(order);
 
-		//使用fastJson将对象转化为Json
+        //使用fastJson将对象转化为Json
 //		String json = JSON.toJSONString(order);
 //		mongoTemplate.insert(json, "class");
 
@@ -60,16 +50,16 @@ public class MyShardingApplicationTests {
 //		orderService.insert(order);
 //		System.out.println(orderService.findAll());
 
-		//条件查询 地址 https://blog.csdn.net/weixin_39214304/article/details/84791953
-		order.setOrderId(8992584491376641l);
-		Example<TOrder> example = Example.of(order);
+        //条件查询 地址 https://blog.csdn.net/weixin_39214304/article/details/84791953
+        order.setOrderId(8992584491376641l);
+        Example<TOrder> example = Example.of(order);
 
-		//排序
-		Sort sort = new Sort(Sort.Direction.DESC,"createTime");
-		List<TOrder> all = orderService.findAll(sort);
+        //排序
+        Sort sort = new Sort(Sort.Direction.DESC, "createTime");
+        List<TOrder> all = orderService.findAll(sort);
 
-		System.out.println("result=== "+all);
+        System.out.println("result=== " + all);
 
-	}
+    }
 
 }
