350d5fa0e345164f09c5b498aa0c1cda18684b34
pukaiquan <1137580844@qq.com>
2019-07-17 11:42:59 +0800

    采坑

--- src/test/java/com/study/sharding/MyShardingApplicationTests.java
+++ src/test/java/com/study/sharding/MyShardingApplicationTests.java
@@ -51,6 +51,15 @@ public class MyShardingApplicationTests {
 //		System.out.println(orderService.findAll());
 
         //条件查询 地址 https://blog.csdn.net/weixin_39214304/article/details/84791953
+
+        //如果实体类中包含有基本数据类型的属性，那么在使用repository.find(Example)时，
+        // 需要把这些属性忽略掉，因为基本数据类型在新建对象时会有默认值，这时如果你按照
+        // 别的属性查找数据时，这些属性也会附带到条件里。比如上文代码中有一个根据用户名
+        // 查找用户的方法，此时你期望的是在查询过程中只有用户名起作用，那么这时候就要加
+        // 上下面的代码。如果不加，则条件中会多出age=0&createTime=0，查出的结果有误。
+        //ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("age","createTime");
+        //Example<User> example = Example.of(user, matcher);
+
         order.setOrderId(8992584491376641l);
         Example<TOrder> example = Example.of(order);
 
